6571d5235e7395dbebcd4e5d48bf705810da1bad
JeremyC <jeremy4322@gmail.com>
2022-08-30 10:34:16 -0400

    netlify test

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ const Header = ({ handleMovieInput, handleSubmit, movieInput, newMovieInput }) =
 
     return (
         <header>
-            <h1 className="animate__animated animate__fadeInUp">Quick Flick Picker</h1>
+            <h1 className="animate__animated animate__fadeInUp">Quick Flick Test</h1>
             <p className="animate__animated animate__fadeInUp">Search for a movie, create a personalized movie list, and add movies to it, and then pick your genre and movie length and let us pick a quick flick for you!</p>
             <form action="" onSubmit={handleSubmit} className="animate__animated animate__fadeInUp">
                 <label
